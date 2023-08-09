7c10a3ee0d9460c46d88d20a030471f2f5f48e81
catolivetree <catarina31oliveira@hotmail.com>
2023-08-09 22:27:47 +0200

    metrics overview - done!

--- player_types.py
+++ player_types.py
@@ -1,58 +1,105 @@
-import requests, json
+import json
+from statistics import mean, pstdev
 
-metrics={
+
+def parse_json(response) -> dict:
+    metrics_per_session={
     "SCORES":[], #each position of the list will represent the score achieved after a playthrough
     "PLAYTIMES":[], #each position of the list will represent how long a session took
     "DAYS_PLAYED":[],  #each position of the list will represent the amount of simulated days the player decided to select for the session
     "HOME_PATH":[], #each position of the list will represent the amount of times a player chose a Home Path Type during a session
     "WORK_PATH":[], #each position of the list will represent the amount of times a player chose a Home Path Type during a session
     "OUTDOORS_PATH":[] #each position of the list will represent the amount of times a player chose a Home Path Type during a session
-    } 
+    }
 
-url = "https://api3-new.gamebus.eu/v2/players/993/activities?gds=SUGARVITA_PLAYTHROUGH"
+    parsed_response=json.loads(response.text)
+    #parsed_response=parsed_response[-1] #last record
 
-payload={}
-headers = {
-  'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiZ2FtZWJ1c19hcGkiXSwidXNlcl9uYW1lIjoiZC52YW5laWpsQHN0dWRlbnQuZm9udHlzLm5sIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIiwidHJ1c3QiXSwiZXhwIjoxNzY3OTQyODcxLCJhdXRob3JpdGllcyI6WyJERVYiLCJVU0VSIl0sImp0aSI6InQ3OUZnQm4xaGprX3BnSG1CQ0NPOVVXc3BEayIsImNsaWVudF9pZCI6ImdhbWVidXNfYmFzZV9hcHAifQ.Ey9WBijTTCoB5WdmiWb_pfrBOPUHxSh0b8jGlWzPqK3ahTbkGdGvG5alm3Tl75cse3RVSq7Y-XttuoStQSXMJpLTEMjXFCuqEbkp-wvnl-xepguWutGECaFJy0XxlGUTOfBYe8Zahl7sN6TTH3h0aZYw2LD60qHEPdcL3bpeW0NBcq4um_50E-0mHdgtxQWzblVy6fr5itjmI-4azSlr2XOYyamNYNmsjfnfHQPNq1RYhFpy-ewXc7s1svFh9EhOMd5OcWD0ht5cDRcm6Iqz6T06W6az05fIWlXN2Q9k5SzPu0Ct0YBkzr4EXxwXyJeT-nWZnCbi30wYwEd78FtsJw'
-  }
+    for record in parsed_response:
+        try:
+            metrics_per_session['SCORES'].append(int(record['propertyInstances'][0]['value']))
+        except:
+            metrics_per_session['SCORES'].append('NaN')
 
-response = requests.request("GET", url, headers=headers, data=payload)
-parsed_response=json.loads(response.text)
-print(type(parsed_response))
-parsed_response=parsed_response[-1] #last record
+        try:
+            metrics_per_session['PLAYTIMES'].append(int(record['propertyInstances'][1]['value']))
+        except:
+            metrics_per_session['PLAYTIMES'].append('NaN')
 
-metrics['SCORES'].append(parsed_response['propertyInstances'][0]['value'])
-metrics['PLAYTIMES'].append(parsed_response['propertyInstances'][1]['value'])
-playthrough_data=parsed_response['propertyInstances'][4]['value']
-#print(type(playthrough_data)) #"dictonary" string
-playthrough_data=json.loads(playthrough_data) #converts the "dictionary" string into a dictionary
-#print(type(playthrough_data)) #dictonary
-metrics['DAYS_PLAYED'].append(playthrough_data['daysPlayed'])
+        try:
+            playthrough_data=record['propertyInstances'][4]['value']
+            playthrough_data=json.loads(playthrough_data) #converts the "dictionary" string into a dictionary
+            metrics_per_session['DAYS_PLAYED'].append(playthrough_data['daysPlayed'])
+            
+            home_path, outdoors_path, work_path = 0, 0, 0
 
-home_path, outdoors_path, work_path = 0, 0, 0
+            for turn in playthrough_data['turns']:
+                if turn['DestinationPathType'] == 1:
+                    home_path+=1
+                elif turn['DestinationPathType'] == 2:
+                    outdoors_path+=1
+                elif turn['DestinationPathType']==3:
+                    work_path+=1
 
-for turn in playthrough_data['turns']:
-    if turn['DestinationPathType'] == 1:
-        home_path+=1
-    elif turn['DestinationPathType'] == 2:
-        outdoors_path+=1
-    elif turn['DestinationPathType']==3:
-        work_path+=1
-
-metrics['HOME_PATH'].append(home_path)
-metrics['WORK_PATH'].append(work_path)
-metrics['OUTDOORS_PATH'].append(outdoors_path)
-
-print(metrics)
+            metrics_per_session['HOME_PATH'].append(home_path)
+            metrics_per_session['WORK_PATH'].append(work_path)
+            metrics_per_session['OUTDOORS_PATH'].append(outdoors_path)
+        
+        except:
+            metrics_per_session['HOME_PATH'].append('NaN')
+            metrics_per_session['WORK_PATH'].append('NaN')
+            metrics_per_session['OUTDOORS_PATH'].append('NaN')
+            metrics_per_session['DAYS_PLAYED'].append('NaN')
+        
+    return metrics_per_session
 
 
-                          
+def remove_nan(metrics) -> list: 
+    # using list comprehension to perform the task
+    for key, value in metrics.items():
+        value_cleaned = [i for i in value if i != 'NaN']
+        metrics[key]=value_cleaned
+    return metrics
 
 
+def manipulate_initial_metrics(metrics_cleaned) -> dict:
+    metrics_overview={
+        "avg_score":0, #average of the scores from all sessions played
+        "sd_score":0, #standard deviation """
+        "avg_playtimes":0, #average of the playtimes """
+        "sd_playtimes":0, #standar deviation """
+        "1_day_session":0, #total of sessions that simulated 1 day in the game
+        "2_days_session":0, #"""" 2 days """
+        "3_days_session":0, #""" 3 days """
+        "avg_days_session":0, #average days sinulated in the game
+        "sd_days_session":0, #standard deviation...
+        "total_work_path":0, #total times a player as chosen a work path
+        "total_home_path":0, #""" home path
+        "total_outdoors_path":0 #""" outdoors path
+    }
 
-#id_player=parsed_response['creator']['id']
-#timestamp=parsed_response['date']
-#activities=parsed_response['propertyInstances'] #activities[0]-> related to the type of emotions; activities[1] -> related to the intensity.
-#type_emotion=(activities[0]['value'])
-#intensity_emotion=(activities[1]['value'])
-#print(id_player, timestamp, type_emotion, intensity_emotion)
+    for key, value in metrics_cleaned.items():
+        if key=='SCORES':
+            metrics_overview['avg_score']=round(mean(value),2)
+            metrics_overview['sd_score']=round(pstdev(value),2)
+        elif key=='PLAYTIMES':
+            metrics_overview['avg_playtimes']=round(mean(value),2)
+            metrics_overview['sd_playtimes']=round(pstdev(value),2)
+        elif key=='DAYS_PLAYED':
+            metrics_overview['avg_days_session']=round(mean(value),2)
+            metrics_overview['sd_days_session']=round(pstdev(value),2)
+            for days in value:
+                if days==1:
+                    metrics_overview['1_day_session']+=1
+                elif days==2:
+                    metrics_overview['2_days_session']+=1
+                elif days==3:
+                    metrics_overview['3_days_session']+=1
+        elif key=="HOME_PATH":
+            metrics_overview['total_home_path']=sum(value)
+        elif key=="WORK_PATH":
+            metrics_overview['total_work_path']=sum(value)
+        elif key=='OUTDOORS_PATH':
+            metrics_overview['total_outdoors_path']=sum(value)
+    
+    return metrics_overview
